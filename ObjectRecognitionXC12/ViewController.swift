4c88a5f46649a96b4872a7d5c36878eae4444fa9
Harshit Jain <harshit120299@gmail.com>
2021-06-18 01:24:42 +0530

    get results of VNCoreMLRequest

--- ObjectRecognitionXC12/ViewController.swift
+++ ObjectRecognitionXC12/ViewController.swift
@@ -6,12 +6,57 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage]
+        if let pickedImage  = image as? UIImage {
+            imageView.image = pickedImage
+            guard let ciimage = CIImage(image: pickedImage) else {
+                fatalError("Couldn't Convert to CIImage")
+            }
+            detect(ciimage: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(ciimage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3(configuration: .init()).model) else {
+            fatalError(" error: loading coreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("request failed CoreML")
+            }
+            print(results)
+        }
+        let handler = VNImageRequestHandler(ciImage: ciimage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
 
